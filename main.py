c28bca8cff1e3d48bfa25e64a1ab2a339bdfc77a
mrinmoy <mrinmoy.low2002@gmail.com>
2025-02-03 20:51:15 +0530

    This is the commit before ingestion

---
+++ main.py
@@ -0,0 +1,3 @@
+from cnnClassifier import logger
+
+logger.info("Welcome to my custom log")  #All the logs are saved in the logs folder
---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+# Here we will mention those things that we will be doing only one time like reading yaml and config file
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
